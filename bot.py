2e2fa81311cc450bd75256134b5b947cf15d69d1
BitterBeast <141508197+BitterBeast@users.noreply.github.com>
2023-08-21 14:17:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=90,
+            workers=50,
             plugins={"root": "plugins"},
             sleep_threshold=5,
         )
