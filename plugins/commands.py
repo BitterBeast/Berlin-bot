483b7fd394c6f6f5607e53e3aaa8541e6ae2f494
BitterBeast <141508197+BitterBeast@users.noreply.github.com>
2024-03-08 22:31:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -58,36 +58,19 @@ async def start(client, message):
             parse_mode=enums.ParseMode.HTML,
             disable_web_page_preview=True
         )
-        return
-    if AUTH_CHANNEL and not await is_subscribed(client, message):
-        try:
-            invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
-        except ChatAdminRequired:
-            logger.error("Make sure Bot is admin in Forcesub channel")
+       return
+    
+    if len(message.command) == 2 and message.command[1] in [
+        "subscribe",
+        "error",
+        "okay",
+        "help",
+        "start",
+        "hehe",
+    ]:
+        if message.command[1] == "subscribe":
+            await ForceSub(client, message)
             return
-        btn = [
-            [
-                InlineKeyboardButton(
-                    "🤖 Join Updates Channel", url=invite_link.invite_link
-                )
-            ]
-        ]
-
-        if message.command[1] != "subscribe":
-            try:
-                kk, file_id = message.command[1].split("_", 1)
-                pre = 'checksubp' if kk == 'filep' else 'checksub' 
-                btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
-            except (IndexError, ValueError):
-                btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
-        await client.send_message(
-            chat_id=message.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**",
-            reply_markup=InlineKeyboardMarkup(btn),
-            parse_mode=enums.ParseMode.MARKDOWN
-            )
-        return
-    if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
             InlineKeyboardButton('➕ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
@@ -104,8 +87,18 @@ async def start(client, message):
             parse_mode=enums.ParseMode.HTML,
             disable_web_page_preview=True
         )
+         return
+
+    kk, file_id = (
+        message.command[1].split("_", 1)
+        if "_" in message.command[1]
+        else (False, False)
+    )
+    pre = ("checksubp" if kk == "filep" else "checksub") if kk else False
+
+    status = await ForceSub(client, message, file_id=file_id, mode=pre)
+    if not status:
         return
-    data = message.command[1]
     try:
         pre, file_id = data.split('_', 1)
     except:
