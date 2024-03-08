905eba09e64d87643ecd8ff36d05884d0b3ef1c4
BitterBeast <141508197+BitterBeast@users.noreply.github.com>
2024-03-08 09:57:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'GreyMatters'
+def lotta_py():
+    return 'Muhammad-Inan'
 
 
 if __name__ == "__main__":
