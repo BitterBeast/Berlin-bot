1226001676fa75d5a25a6c522bcb9e532ac5430b
BitterBeast <muhammadzinanx@gmail.com>
2024-03-10 01:10:28 +0530

    update Script.py

--- Script.py
+++ Script.py
@@ -100,7 +100,7 @@ This module only works for my admins
 ★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
 ★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
 ★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-UPTIME :<code>{uptime}</code>"""
+UPTIME :<code>{}</code>"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
