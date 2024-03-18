8a8401290178ec894815bfe546c3282fd397f31b
BitterBeast <muhammadzinanx@gmail.com>
2024-03-18 10:00:31 +0530

    update

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -3,6 +3,7 @@ import asyncio
 import re
 import ast
 import math
+import time
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 import pyrogram
@@ -13,7 +14,7 @@ from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GRO
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
-from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
+from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings, get_time
 from database.users_chats_db import db
 from database.ia_filterdb import Media, get_file_details, get_search_results
 from database.filters_mdb import (
@@ -29,6 +30,7 @@ logger.setLevel(logging.ERROR)
 BUTTONS = {}
 SPELL_CHECK = {}
 SPELL_MODE = "True"
+START_TIME = time.time()
 
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
@@ -548,6 +550,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         chats = await db.total_chat_count()
         monsize = await db.get_db_size()
         free = 536870912 - monsize
+        uptime = get_time(time.time() - START_TIME)
         monsize = get_size(monsize)
         free = get_size(free)
         await query.message.edit_text(
